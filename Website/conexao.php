897c92f49831338342f060b3ee0bc3f01cd6e5d4
Igor <106703317+IgorBrito12k@users.noreply.github.com>
2023-02-25 12:52:00 -0300

    docker do indiano

--- Website/conexao.php
+++ Website/conexao.php
@@ -4,16 +4,15 @@
     $dbtype   = "mysql";
     $host     = "localhost";
     $port     = "3306";
-    $user     = "user";
-    $password = "senha";
-    $db       = "hospital";
+    $user     = "maria_db";
+    $password = "root";
+    $db       = "trabalholp2";
 
-    try {  
-        $conexao = new PDO($dbtype.":host=".$host.
-        ";port=".$port.";dbname=".$db, $user, $password);
-            echo "<p>Banco de dados conectado.</p>";
-        } catch (PDOException $i) {
-        //se houver exceção, exibe
-            die("Erro: <code>" . $i->getMessage() . "</code>");
+    $conexao = new mysqli('mysql_db', 'root', 'root', 'mysql');
+    
+    if($conexao) {
+        echo "Conectado!!";
+    } else {
+        echo "Não conectado!";
     }
 ?>
