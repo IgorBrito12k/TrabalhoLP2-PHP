9dc7b4050af2afc05c2155816a23d6b79fed9ae0
Igor <106703317+IgorBrito12k@users.noreply.github.com>
2023-02-25 19:17:58 -0300

    Modificado index.php e add css

--- Website/index.php
+++ Website/index.php
@@ -1,5 +1,47 @@
-<h1>Ola igor</h1>
-<h4>Testando o php...</h4>
-<?php
-  echo "<p>Hello World</p>";
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="cadastre-se">
+          <div class="convite-cadastro">
+            <h1>Olá Amigo!</h1>
+            <p>Se você não possui conta, cadastre-se agora e se junte a nossa jornada!</p>
+            <button class="botao-cadastre-se">Cadastre-se</button>
+          </div>
+        </div>  
+        <!-- criar a div formularios -->
+        <div class="container-formulario">
+            <form method="" action="">
+                <div class="cabecalho-formulario">
+                    <h1>Login</h1>
+                </div>
+                <div class="grupo-input">
+                    <div class="input-box">
+                        <label for="">Email</label>
+                        <input class="email required" type="email" name="email" placeholder="Digite seu email">
+                    </div>
+
+                    <div class="input-box">
+                        <label for="">Senha</label>
+                        <input class="nasc required" type="password" name="senha" placeholder="Digite sua senha">
+                    </div>
+                </div>
+                <a href="">Esqueceu sua senha?</a><br>
+                <button class="botao-logar">Logar</button>
+            </form>
+        </div>
+    </div>
+    
+    <script></script>
+</body>
+
+</html>
